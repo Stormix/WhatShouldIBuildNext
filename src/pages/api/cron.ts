187b5c5420ea1eaa188dd9e3f5a13707c3c6bcc2
Anas Mazouni <madadj4@gmail.com>
2023-04-08 05:36:36 +0200

    feat: daily credits for active users WHA-12

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,11 +1,28 @@
+import { prisma } from '@/server/db';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   console.log('Cron job ran at: ', new Date().toISOString());
 
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies
+  const users = await prisma.user.updateMany({
+    where: {
+      sessions: {
+        some: {
+          expires: {
+            gte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
+          }
+        }
+      }
+    },
+    data: {
+      credits: {
+        increment: 1
+      }
+    }
+  });
+
+  return response.status(200).json({
+    message: 'Cron job ran successfully',
+    users: users.count
   });
 }
