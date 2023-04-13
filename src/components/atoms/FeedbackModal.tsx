aa79072be35f79b600fe234a19d1ef51c0812221
Anas Mazouni <madadj4@gmail.com>
2023-04-13 15:06:25 +0200

    fix: build

--- src/components/atoms/FeedbackModal.tsx
+++ src/components/atoms/FeedbackModal.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { env } from '@/env.mjs';
-import Feedback from 'feeder-react-feedback';
 import 'feeder-react-feedback/dist/feeder-react-feedback.css';
+import dynamic from 'next/dynamic';
+
+const Feedback = dynamic<{ projectId: string }>(() => import('feeder-react-feedback'), {
+  ssr: false
+});
 
 const FeedbackModal = () => {
   if (!env.NEXT_PUBLIC_FEEDER_PROJECT_ID) return null;
