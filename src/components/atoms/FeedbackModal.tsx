ce762999e03faa1df0bce1e282e3407d91d705e5
Anas Mazouni <madadj4@gmail.com>
2023-04-13 15:19:47 +0200

    fix: styling and add email input

--- src/components/atoms/FeedbackModal.tsx
+++ src/components/atoms/FeedbackModal.tsx
@@ -4,13 +4,16 @@ import { env } from '@/env.mjs';
 import 'feeder-react-feedback/dist/feeder-react-feedback.css';
 import dynamic from 'next/dynamic';
 
-const Feedback = dynamic<{ projectId: string }>(() => import('feeder-react-feedback'), {
-  ssr: false
-});
+const Feedback = dynamic<{ projectId: string; email: boolean; postSubmitButtonMsg: string }>(
+  () => import('feeder-react-feedback'),
+  {
+    ssr: false
+  }
+);
 
 const FeedbackModal = () => {
   if (!env.NEXT_PUBLIC_FEEDER_PROJECT_ID) return null;
-  return <Feedback projectId={env.NEXT_PUBLIC_FEEDER_PROJECT_ID} />;
+  return <Feedback projectId={env.NEXT_PUBLIC_FEEDER_PROJECT_ID} email={true} postSubmitButtonMsg={`Thank you!`} />;
 };
 
 export default FeedbackModal;
